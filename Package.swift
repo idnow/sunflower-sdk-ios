eaba809f12334957ece40899ec43c40ae4878810
sunflower-developer <sunflower-developer@idnow.io>
2025-07-07 09:18:05 +0200

    Release - Update SDK to version 1.4.2

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "Sunflower",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "SunflowerUIKit",
+            targets: ["SunflowerUIKitWrapper"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/airbnb/lottie-spm.git", exact: "4.5.2")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "SunflowerUIKit",
+            url: "https://github.com/idnow/sunflower-sdk-ios/releases/download/1.4.2/SunflowerUIKit.xcframework.zip",
+            checksum: "fa2aa262a8238b2d4fe0955059c1d81adf56ea312108ef2b31b10e8f07630d8c"
+        ),
+        .target(
+             // Main target which contains both Sunflower and the lottie dependency. Automatically downloaded when client fetch Sunflower.
+            name: "SunflowerUIKitWrapper",
+            dependencies: [
+                "SunflowerUIKit",
+                .product(name: "Lottie", package: "lottie-spm")
+            ],
+            path: "sources"
+        )
+    ]
+)
