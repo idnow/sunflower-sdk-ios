09833c1ed7fcddec967df4ab67023429835e684a
sunflower-developer <sunflower-developer@idnow.io>
2025-12-24 11:41:06 +0100

    Release - Update SDK to version 2.1.2

--- Package.swift
+++ Package.swift
@@ -23,13 +23,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SunflowerUIKit",
-            url: "https://github.com/idnow/sunflower-sdk-ios/releases/download/2.1.1/SunflowerUIKit.xcframework.zip",
-            checksum: "bb7f443278d7dbcd6ccc154f685767b442d523934b007b3eca25e90fd6156ebb"
+            url: "https://github.com/idnow/sunflower-sdk-ios/releases/download/2.1.2/SunflowerUIKit.xcframework.zip",
+            checksum: "19ef97651291a34f15c8b3bd8324029cc14e55623470ac65b7aa21b795169304"
         ),
         .binaryTarget(
             name: "SunflowerSwiftUI",
-            url: "https://github.com/idnow/sunflower-sdk-ios/releases/download/2.1.1/SunflowerSwiftUI.xcframework.zip",
-            checksum: "b325b6dbcc6e4c2cd0d896188d89c20976a3c88276fd35a4df277d49b2f31edf"
+            url: "https://github.com/idnow/sunflower-sdk-ios/releases/download/2.1.2/SunflowerSwiftUI.xcframework.zip",
+            checksum: "060aee3f3f34d08dd6ef6a1b9f0ffb06958d5e4cea6921b7d53c5d1f1b49685c"
         ),
         .target(
              // Main target which contains both SunflowerUIKit and the lottie dependency. Automatically downloaded when client fetch Sunflower.
